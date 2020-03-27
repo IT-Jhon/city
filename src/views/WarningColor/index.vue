32ca340abcd6535023f402d4db959e4d6a52c719
seven00715 <1027369812@qq.com>
2020-03-27 23:55:36 +0800

    更新错误

--- src/views/WarningColor/index.vue
+++ src/views/WarningColor/index.vue
@@ -152,12 +152,12 @@ export default {
     // 全部
     all() {
       this.tableData = [
-        { checkbox: false, id: 1, max: 3, min: 0, color: '#ff0000', remark: '立案数较少', type: '小' },
-        { checkbox: false, id: 2, max: 6, min: 4, color: '#00ff00', remark: '立案数较少', type: '小' },
-        { checkbox: false, id: 3, max: 9, min: 7, color: '#0000ff', remark: '立案数较少', type: '中' },
-        { checkbox: false, id: 4, max: 15, min: 10, color: '#ffff00', remark: '立案数较少', type: '中' },
-        { checkbox: false, id: 5, max: 20, min: 16, color: '#ff00ff', remark: '立案数较少', type: '大' },
-        { checkbox: false, id: 6, max: 600, min: 21, color: '#00ffff', remark: '立案数较少', type: '特大' }
+        { checkbox: false, id: 1, max: 3, min: 0, color: { name: 'Red', color: '#ff0000' }, remark: '立案数较少' },
+        { checkbox: false, id: 2, max: 6, min: 4, color: { name: 'Green', color: '#00ff00' }, remark: '立案数较少' },
+        { checkbox: false, id: 3, max: 9, min: 7, color: { name: 'Blue', color: '#0000ff' }, remark: '立案数较少' },
+        { checkbox: false, id: 4, max: 15, min: 10, color: { name: 'Yellow', color: '#ffff00' }, remark: '立案数较少' },
+        { checkbox: false, id: 5, max: 20, min: 16, color: { name: 'purple', color: '#ff00ff' }, remark: '立案数较少' },
+        { checkbox: false, id: 6, max: 600, min: 21, color: { name: 'Cyan', color: '#00ffff' }, remark: '立案数较少' }
 
       ]
     },
